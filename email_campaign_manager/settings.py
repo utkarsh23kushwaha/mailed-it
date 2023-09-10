a1d7ca32211213d5bdfee56332a2d0385a2ea1fc
Utkarsh Kushwaha <72360686+utkarsh23kushwaha@users.noreply.github.com>
2023-09-10 11:37:35 +0530

    Update settings.py

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
