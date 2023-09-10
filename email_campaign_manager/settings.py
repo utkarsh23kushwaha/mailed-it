ddbcaf605641e106edfbdd7d210cb81809bc64b9
Utkarsh Kushwaha <72360686+utkarsh23kushwaha@users.noreply.github.com>
2023-09-10 11:21:36 +0530

    Update settings.py

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
+
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
 
 # Default primary key field type
