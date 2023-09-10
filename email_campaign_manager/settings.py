04d9a7837e42a312b7dcf2862120fd63c3e9e782
Utkarsh Kushwaha <72360686+utkarsh23kushwaha@users.noreply.github.com>
2023-09-10 11:30:38 +0530

    Update settings.py

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
+
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
 
