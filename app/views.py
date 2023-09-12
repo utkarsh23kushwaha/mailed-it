5561f858996e9ae2f043b255470d9752fd18535c
Utkarsh Kushwaha <72360686+utkarsh23kushwaha@users.noreply.github.com>
2023-09-12 23:29:11 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -6,29 +6,64 @@ from django.contrib import messages
 from django.template.loader import render_to_string
 from app.utils import send_email_with_template
 from app.utils import sheet_parser
-from django_user_agents.utils import get_user_agent
 from django.contrib.auth.decorators import login_required, user_passes_test
 import pandas as pd
+from django_user_agents.utils import get_user_agent
 
 
+def is_admin(user):
+    return user.is_staff
 
 def main(request):
-    user_agent = get_user_agent(request)
-    if user_agent.is_mobile or user_agent.is_tablet: 
-        template_name = 'mobile.html'
-    else : 
-        template_name = 'base.html'
-    return render (request, template_name)
+    return render (request, "base.html")
 
 def send_mail(request):
     return render(request, 'send_mail_alt.html')
+
+
+# @login_required  # Ensures the user is logged in
+# @user_passes_test(is_admin)  # Ensures the user passes the custom test
+# def send_mail(request):
+#     template_path  = 'mail_template.html' 
+#     if request.method == 'POST':
+#         subject = request.POST.get('subject')
+#         cap = Campaign.objects.filter(subject = subject)
+#         for campaign in cap:
+#             subject = campaign.subject
+#             preview_text = campaign.preview_text
+#             article_url = campaign.article_url
+#             html_content = campaign.html_content
+#             plain_text_content = campaign.plain_text_content
+#             published_date = campaign.published_date
+#         campaign_data = {
+#                             'Subject': subject,
+#                             'preview_text': preview_text,
+#                             'article_url': article_url,
+#                             'html_content': html_content,
+#                             'plain_text_content': plain_text_content,
+#                             'published_date': published_date
+#                         }
+#         rendered_email = render_to_string('mail_template.html', context = campaign_data)
+#         recipients = Subscriber.objects.filter(Status='active').values_list('email', flat=True)
+   
+#         if send_email_with_template(subject, rendered_email, recipients):
+#             return redirect('success_page')
+#         else:
+#             return HttpResponse("Mail Sending Failed :(")
+
+#     subjects = Campaign.objects.values_list('subject', flat=True).distinct()
+#     return render(request, 'send_mail.html', {'subjects': subjects})
+
+def success_page(request):
+    return render(request, 'success.html')
 
 def import_subscribers(request):
 
     if request.method == 'POST':
         link = request.POST.get('link')
-        print(link)
-        data = sheet_parser(link)
+        name = request.POST.get('sheet_name')
+        data = sheet_parser(link,name)
+        print(data)
         email_var_list = ["email","emails","e-mails","e-mail"]
         columns_list = data.columns.tolist()
         print(columns_list)
@@ -36,7 +71,7 @@ def import_subscribers(request):
         name = None
         try :
             for item in columns_list:
-                if "name" in item.lower():
+                if "name" in item.lower() and 'unnamed' not in item.lower():
                     name=item
                     
         except Exception as e:
@@ -73,72 +108,33 @@ def import_subscribers(request):
                         else :
                             subs = Subscriber(name=name, email=email, Subscribed_date=date.date(), Status="active", Unsubscribed_date=None)
                             subscriber_list.append(subs)
+                            
                 
                 else:
-                    messages.error(request, "Error occured, check the data in your sheet")
+                    messages.error(request, "Error occured,number of names not equal to the number of emails")
             except Exception as e:
-                messages.error(request, "Error occured, check the data in your sheet")
+                messages.error(request, f"Error occured, check the data in your sheetf{e}")
 
-        
-            Subscriber.objects.bulk_create(subscriber_list)
-            messages.success(request, "Subsribers sucessfully imported from Google Sheet")
-            name_list = []
-            email_list = []
+            if len(subscriber_list)>=1:
+
+                Subscriber.objects.bulk_create(subscriber_list)
+                messages.success(request, "Subsribers sucessfully imported from Google Sheet")
+                name_list = []
+                email_list = []
+            else:
+                messages.success(request, "Skipped Duplicates, Import Successful")
+
+
+
 
     return render(request, 'import_sheets.html')
 
-
-# def send_mail(request):
-#     user_agent = get_user_agent(request)
-#     if user_agent.is_mobile or user_agent.is_tablet: 
-#         template_name = 'mobile.html'
-#     else : 
-#         template_name = 'send_mail.html'
-#     if request.method == 'POST':
-#         subject = request.POST.get('subject')
-#         cap = Campaign.objects.filter(subject = subject)
-#         for campaign in cap:
-#             subject = campaign.subject
-#             preview_text = campaign.preview_text
-#             article_url = campaign.article_url
-#             html_content = campaign.html_content
-#             plain_text_content = campaign.plain_text_content
-#             published_date = campaign.published_date
-#         campaign_data = {
-#                             'Subject': subject,
-#                             'preview_text': preview_text,
-#                             'article_url': article_url,
-#                             'html_content': html_content,
-#                             'plain_text_content': plain_text_content,
-#                             'published_date': published_date
-#                         }
-#         rendered_email = render_to_string('mail_template.html', context = campaign_data)
-#         recipients = Subscriber.objects.filter(Status='active').values_list('email', flat=True)
    
-#         if send_email_with_template(subject, rendered_email, recipients):
-#             return redirect('success_page')
-#         else:
-#             return HttpResponse("Mail Sending Failed :(")
-
-#     subjects = Campaign.objects.values_list('subject', flat=True).distinct()
-#     return render(request, template_name, {'subjects': subjects})
-
-def success_page(request):
-    return render(request, 'success.html')
-
-def mobile(request):
-    return render(request, 'mobile.html')
 
 def add_subscribe(request):
-    user_agent = get_user_agent(request)
-    if user_agent.is_mobile or user_agent.is_tablet: 
-        template_name = 'mobile.html'
-    else : 
-        template_name = 'add_subscribe.html'
     if request.method == 'POST':
         name = request.POST.get('name')
-        email = request.POST.get('email')
-        
+        email = request.POST.get('email')        
         try:
            
             subscriber = Subscriber.objects.get(email=email)
@@ -157,14 +153,9 @@ def add_subscribe(request):
             subs = Subscriber(name=name, email=email, Subscribed_date=date.date(), Status="active", Unsubscribed_date=None)
             subs.save()
             messages.success(request, "Subscriber Added Successfully.")
-    return  render(request, template_name)
+    return  render(request, 'add_subscribe.html')
 
 def unsubscribe(request):
-    user_agent = get_user_agent(request)
-    if user_agent.is_mobile or user_agent.is_tablet: 
-        template_name = 'mobile.html'
-    else : 
-        template_name = 'unsubscribe.html'
     if request.method=='POST':
         email  = request.POST.get('email')
         try:
@@ -180,14 +171,9 @@ def unsubscribe(request):
 
         except Subscriber.DoesNotExist:
             messages.error(request, f"Subscriber with email {email} not found.")
-    return render (request, template_name)
+    return render (request, "unsubscribe.html")
 
 def add_campaign(request):
-    user_agent = get_user_agent(request)
-    if user_agent.is_mobile or user_agent.is_tablet: 
-        template_name = 'mobile.html'
-    else : 
-        template_name = 'add_campaign.html'
     if request.method=='POST':
         subject = request.POST.get('subject')
         preview_text = request.POST.get('preview_text')
@@ -204,4 +190,4 @@ def add_campaign(request):
         cap.save()
 
 
-    return render (request, template_name)
+    return render (request, "add_campaign.html")
